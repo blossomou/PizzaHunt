c54b81d4dca2a82a9f00b79f610abf9fa00024c1
QingOu <qingqingou@gmail.com>
2022-03-29 09:55:53 -0400

    Add addcomment and remove in Comment controllers

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,25 +1,43 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    size: {
+      type: String,
+      default: 'Large',
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment',
+      },
+    ],
   },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  size: {
-    type: String,
-    default: "Large",
-  },
-  toppings: [],
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+PizzaSchema.virtual('commentCount').get(function () {
+  return this.comments.length;
 });
 
 // create the Pizza model using PizzaSchema
-const Pizza = model("Pizza", PizzaSchema);
+const Pizza = model('Pizza', PizzaSchema);
 
 // export the Pizza model
 module.exports = Pizza;
