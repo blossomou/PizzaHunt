b59f808b7b4a46a2acfe9e9c55debf3720cceced
QingOu <qingqingou@gmail.com>
2022-03-29 10:24:56 -0400

    wire get pizzalist in client and format date

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,3 +1,4 @@
+const dateFormat = require('../utils/dateFormat');
 const { Schema, model } = require('mongoose');
 
 const PizzaSchema = new Schema(
@@ -11,6 +12,7 @@ const PizzaSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
     size: {
       type: String,
@@ -27,6 +29,7 @@ const PizzaSchema = new Schema(
   {
     toJSON: {
       virtuals: true,
+      getters: true,
     },
     id: false,
   }
