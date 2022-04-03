368878ea5dd8171259b7858fcb90b9ef43c47724
QingOu <qingqingou@gmail.com>
2022-04-03 18:18:35 -0400

    add .env package

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use(require("./routes"));
+app.use(require('./routes'));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -19,5 +20,5 @@ mongoose.connect(
 );
 
 // Use this to log mongo queries being executed!
-mongoose.set("debug", true);
+mongoose.set('debug', true);
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
