9c8ce5740beba9964e9aa707bafc22134244ae32
QingOu <qingqingou@gmail.com>
2022-04-03 10:49:00 -0400

    Add comment and replies

--- models/Pizza.js
+++ models/Pizza.js
@@ -36,7 +36,10 @@ const PizzaSchema = new Schema(
 );
 
 PizzaSchema.virtual('commentCount').get(function () {
-  return this.comments.length;
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
 });
 
 // create the Pizza model using PizzaSchema
